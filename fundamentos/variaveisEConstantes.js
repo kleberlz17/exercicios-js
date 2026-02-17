8180368501ed77fe7acb180066dd14cba4b5e6d1
Kleber Luiz <157175058+kleberlz17@users.noreply.github.com>
2026-02-17 00:59:24 -0300

    fundamentos

---
+++ fundamentos/variaveisEConstantes.js
@@ -0,0 +1,17 @@
+var a = 3;
+let b = 4;
+
+// optar por uso de let, é mais moderno e mais utilizado.
+var a = 30;
+b = 40;
+
+console.log(a, b);
+
+a = 300;
+b = 400;
+
+console.log(a, b);
+
+const c = 5;
+// c = 50 (nao pode reutilizar o const alterando o valor..)
+console.log(c);
