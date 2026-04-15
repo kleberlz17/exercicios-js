16cca1ad889140739bc67fd8815fceffeb938042
Kleber Luiz <157175058+kleberlz17@users.noreply.github.com>
2026-04-15 01:26:50 -0300

    revisao

---
+++ esnext/revisao1.js
@@ -0,0 +1,21 @@
+// let e const
+{
+  var a = 2;
+  let b = 3;
+  console.log(b);
+}
+console.log(a);
+
+//Template String
+const produto = "IPad";
+console.log(`${produto} é caro!`);
+
+// Destructuring
+const [l, e, ...tras] = "Cod3r";
+console.log(l, e, tras);
+
+const [x, , y] = [1, 2, 3]; // vai ignorar o 2 por causa do espaçamento entre as virgulas
+console.log(x, y);
+
+const { idade: i, nome } = { nome: "Ana", idade: 29 };
+console.log(i, nome);
