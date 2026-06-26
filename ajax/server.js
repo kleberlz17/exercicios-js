555edba1e4442fc8a898e0ad96987fb73e26dea1
Kleber Luiz <157175058+kleberlz17@users.noreply.github.com>
2026-06-25 22:43:23 -0300

    ainda ajax

--- ajax/server.js
+++ ajax/server.js
@@ -12,8 +12,29 @@ app.use(bodyParser.urlencoded({ extended: true }));
 //recebimento de json o transformando em objeto
 app.use(bodyParser.json());
 
-//quando vier requisição pelo /teste...
-app.get("/teste", (req, res) => res.send("Ok"));
+//serve pra interpretar o formulário que vem o arquivo do upload
+const multer = require("multer");
 
+const storage = multer.diskStorage({
+  destination: function (req, file, callback) {
+    callback(null, "./upload");
+  },
+  filename: function (req, file, callback) {
+    callback(null, `${Date.now()}_${file.originalname}`);
+  },
+});
+
+const upload = multer({ storage }).single("arquivo");
+
+app.post("/upload", (req, res) => {
+  upload(req, res, (err) => {
+    //requisiçao, resposta e erro
+    if (err) {
+      return res.end("Ocorreu um erro");
+    }
+
+    res.end("Concluído com sucesso.");
+  });
+});
 //startando servidor..
 app.listen(8080, () => console.log("Executando..."));
