cd3bfe1ebd84079ca503d28c19731303a9045a93
Kleber Luiz <157175058+kleberlz17@users.noreply.github.com>
2026-07-07 19:56:25 -0300

    terminando projeto SPA

--- gulp/spa/gulpTasks/servidor.js
+++ gulp/spa/gulpTasks/servidor.js
@@ -1,11 +1,25 @@
 const gulp = require("gulp");
+const webserver = require("gulp-webserver")
+const watch = require("gulp-watch")
 
 // EXPORTAÇOES DESSE ARQUIVO AQUI TAMBÉM!
-function monitorarArquivos(cb) {
-  return cb();
+//AQUI É PRO SERVIDOR RODAR
+function servidor(cb) {
+  return gulp.src("build")
+    .pipe(webserver({
+      port: 8080,
+      open: true,
+      livereload: true,
+    }))
 }
 
-function servidor(cb) {
+function monitorarArquivos(cb) { 
+  //alterações serão feitas imediatamente
+  watch("src/**/*.html", () => gulp.series("appHTML")())
+  watch("src/**/*.scss", () => gulp.series("appCSS")())
+  watch("src/**/*.js", () => gulp.series("appJS")())
+  watch("src/assets/imgs/**/*.*", () => gulp.series("appIMG")())
+
   return cb();
 }
 
