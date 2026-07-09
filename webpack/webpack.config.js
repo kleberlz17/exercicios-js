4d58790ce63f06e58ec3ffc93888181e9154837a
Kleber Luiz <157175058+kleberlz17@users.noreply.github.com>
2026-07-09 02:58:53 -0300

    finalizando webpack server

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,6 +1,51 @@
 const webpack = require("webpack")
+const MiniCssExtractPlugin = require("mini-css-extract-plugin")
+const modoDev = process.env.NODE_ENV !== "production"
+const TerserPlugin = require("terser-webpack-plugin")
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin")
 
 module.exports = {
-    mode: "development", // se for mode: production o main.js vai ficar bem mais compacto
-    entry: "./src/principal.js"
+    mode: modoDev ? "development" : "production", // se for modoDev = development, se não, production, config no package.json
+    entry: "./src/principal.js",
+    output: {
+        filename: "principal.js",
+        path: __dirname + "/public"
+    },
+
+  devServer: {
+    static: {
+        directory: "./public"
+    },
+    port: 9000,
+    client: {
+        overlay: {
+            errors: true,
+            warnings: false
+        }
+    }
+  },
+
+    optimization: {
+        minimizer: [
+            new TerserPlugin(),
+            new CssMinimizerPlugin()
+        ]
+    },
+
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: "estilo.css"
+        })
+    ],
+    module: {
+        rules: [{
+            test: /\.s?[ac]ss$/, // <<essa expressao encontra css, scss e sass
+            use: [
+                MiniCssExtractPlugin.loader,
+               // "style-loader", // Adiciona CSS a DOM injetando a tag <style>
+                "css-loader", // interpreta imports, url()...
+                "sass-loader"
+            ]
+        }]
+    }
 }
